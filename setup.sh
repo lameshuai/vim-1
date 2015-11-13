73a4a6e6ca6b7a9042246769c7c86b2c6d845803
dudn <18524427628@163.com>
2015-11-14 02:06:12 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,13 @@
 echo "Need to wait a while"
 sudo apt-get install -y vim exuberant-ctags python python-dev cmake build-essential subversion git smbclient tree
 #compile clang and llvm
-rm -rf ~/.vim ; mkdir ~/.vim
+rm -rf ~/.vim
+mkdir ~/.vim
 git clone https://github.com/dudn/vim.git ~/.vim
-mkdir -p ~/vim/build ; cd ~/vim/build
-
+tar jxvf ~/.vim/llvm.tar.bz2 -C ~/.vim
+mkdir -p ~/.vim/build ; cd ~/.vim/build
+cmake -G "Unix Makefiles" ../llvm
+make
 #install vundle
 mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
@@ -15,6 +18,7 @@ echo "BundleInstall" > Temporary
 echo "The installation completes automatically exits" >> Temporary
 vim Temporary -c "BundleInstall" -c "q" -c "q"
 rm Temporary
+#compile youcompleteme
 mkdir -p ~/.vim/ycm_build
 cd ~/.vim/ycm_build
 cmake -G "Unix Makefiles" -DEXTERNAL_LIBCLANG_PATH=~/.vim/ . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
