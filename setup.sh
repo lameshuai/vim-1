40eb02a00cce3548e36372e477f5981709856c63
dudn <18524427628@163.com>
2015-11-14 01:54:36 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 echo "Need to wait a while"
 sudo apt-get install -y vim exuberant-ctags python python-dev cmake build-essential subversion git smbclient tree
-cd ~
-rm -rf ~/.vim ~/.vimrc
-mkdir ~/.vim
+#compile clang and llvm
+rm -rf ~/.vim ; mkdir ~/.vim
+git clone https://github.com/dudn/vim.git ~/.vim
+mkdir -p ~/vim/build ; cd ~/vim/build
+
+#install vundle
 mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-mv -f ~/vim/vimrc ~/.vimrc
-mv -f ~/vim/ycm_extra_conf.py ~/.ycm_extra_conf.py
-mv -f ~/vim/libclang.so ~/.vim/bundle &
+#BundleInstall
+mv -f ~/.vim/vimrc ~/.vimrc
 echo "BundleInstall" > Temporary
 echo "The installation completes automatically exits" >> Temporary
 vim Temporary -c "BundleInstall" -c "q" -c "q"
 rm Temporary
-mkdir -p ~/ycm_build
-cd ~/ycm_build
-cmake -G "Unix Makefiles" -DEXTERNAL_LIBCLANG_PATH=~/.vim/bundle/libclang.so . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+mkdir -p ~/.vim/ycm_build
+cd ~/.vim/ycm_build
+cmake -G "Unix Makefiles" -DEXTERNAL_LIBCLANG_PATH=~/.vim/ . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
 make ycm_core
 make ycm_support_libs
+mv -f ~/.vim/ycm_extra_conf.py ~/.ycm_extra_conf.py
