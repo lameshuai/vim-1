3466b36c0dcf1fe0f2543c780b68800e7a6cde4c
dudn <18524427628@163.com>
2015-11-21 00:27:02 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 echo "Need to wait a while"
 sudo apt-get install -y vim exuberant-ctags python python-dev cmake build-essential subversion git tree gcc g++ flashplugin-installer
-#compile clang and llvm
+#install vundle
 rm -rf ~/.vim
 mv vim ~/.vim
+mkdir -p ~/.vim/bundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+#compile clang and llvm
 tar jxvf ~/.vim/llvm.tar.bz2 -C ~/.vim
 mkdir -p ~/.vim/build 
 cd ~/.vim/build
 cmake -G "Unix Makefiles" ../llvm
 make
-#install vundle
-mkdir -p ~/.vim/bundle
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 #BundleInstall
 mv -f ~/.vim/vimrc ~/.vimrc
-echo "BundleInstall" > ~/Temporary
-echo "The installation completes automatically exits" >> ~/Temporary
-vim ~/Temporary -c "BundleInstall" -c "q" -c "q"
-rm ~/Temporary
+vim  -c "BundleInstall" -c "q" -c "q"
 #compile youcompleteme
 mkdir -p ~/.vim/ycm_build
 cd ~/.vim/ycm_build
